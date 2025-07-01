2811316fc64b59ecd64a44684093bb705a6daf92
aleSuarez999 <alemsuarez@gmail.com>
2025-07-01 17:58:03 -0300

    aside ok con categorias no repetidas

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router'
 import {products} from "../data/products"
+import Text from './Text'
 
 function Aside() {
 
   const categories = products.map(prod => prod.category) // esto trae todas una por registro
+  //const imagenes = products.map(prod => prod.image) // esto trae todas una por registro
   const categories2 = [...new Set(categories)];
-  //const categories = products.filter(prod => (categories[prod.category]) )
 
   console.log(categories2)
-  //return 0
+
   return (
     <aside>
         <nav>
           {
             categories2.map( 
-                            cate => {
-                             <NavLink to = `/products/${cate}` >{cate}</NavLink>
-                            }
-                         )
+                    cate =>   
+                      <div>
+                        <img src='' ></img>
+                        <NavLink key={cate} to={`/products/${cate}`} >{cate}</NavLink>
+                      </div>
+                  )
           }
         </nav>
     </aside>
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -11,8 +11,7 @@ function Layout() {
         <Navbar />
         <Aside />
         <div id='main' className='container'>
-        
-        { <Outlet /> }
+          { <Outlet /> }
         </div>
         <Footer />
     </>
