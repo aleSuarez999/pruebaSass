31dec50387479e86819de4a3d9375c39790d3550
AleSuarez <alemsuarez@gmail.com>
2025-07-29 00:33:36 -0300

    se agrega footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Box from './Box'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import Text from './Text'
 
 function Footer() {
   return (
     <footer className='footer__content' >
         <div className='footer__container'>
-            Footer
+                <Text as="p">Jugueteria Cosmica</Text>
+                <Box className="footer__social">
+                    <FontAwesomeIcon className="footer__socialmedia-links" icon={faFacebook} />
+                    <FontAwesomeIcon className="footer__socialmedia-links" icon={faInstagram} />
+                    <FontAwesomeIcon className="footer__socialmedia-links" icon={faYoutube} />
+                </Box>
         </div>
     </footer>
   )
